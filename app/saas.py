fcbcfd47af8c4a9ca0223fcdf3cba26810c5678f
dejaclarke <dejclark@cisco.com>
2023-09-07 18:07:42 -0700

    finished first layer

--- app/saas.py
+++ app/saas.py
@@ -4,6 +4,8 @@ import re
 from typing import List
 import openai
 
+MAX_INPUT_LENGTH = 32
+
 
 def main():
     print("Running copy kit")
@@ -12,17 +14,26 @@ def main():
     parser.add_argument("--input", "-i", type=str, required=True)
     args = parser.parse_args()
     user_input = args.input
+    if validate_length(user_input):
+        branding_result = generate_branding_snippet(user_input)
+        keywords_result = generate_keywords(user_input)
+        print(branding_result)
+        print(keywords_result)
+    else:
+        raise ValueError(
+            f"Input length is too long. Must be under {MAX_INPUT_LENGTH}. Submitted input is {user_input}"
+        )
 
-    branding_result = generate_branding_snippet(user_input)
-    keywords_result = generate_keywords(user_input)
-    print(branding_result)
-    print(keywords_result)
+
+def validate_length(prompt: str) -> bool:
+    return len(prompt) <= MAX_INPUT_LENGTH
 
 
 def generate_keywords(prompt: str) -> List[str]:
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
     enriched_prompt = f"Generate related branding keywords for {prompt}:"
+    print(enriched_prompt)
 
     response = openai.Completion.create(
         engine="davinci-instruct-beta-v3", prompt=enriched_prompt, max_tokens=32
@@ -44,6 +55,7 @@ def generate_branding_snippet(prompt: str) -> str:
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
     enriched_prompt = f"Generate upbeat branding snippet for {prompt}:"
+    print(enriched_prompt)
 
     response = openai.Completion.create(
         engine="davinci-instruct-beta-v3", prompt=enriched_prompt, max_tokens=32
